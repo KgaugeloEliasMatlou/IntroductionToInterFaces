717c8a8f6318dd28da371a27dcb1be7d9f933c15
KgaugeloEliasMatlou <eliasmatlou66@gmail.com>
2022-06-22 12:27:31 +0200

    changes to interfaces

--- IntroductionToInterFaces/Program.cs
+++ IntroductionToInterFaces/Program.cs
@@ -1,8 +1,74 @@
 ﻿using System;
-class Program
+
+public class Program
 {
     static void Main()
     {
+        Machine1 m1 = new Machine1();
+        Machine2 m2 = new Machine2();
+        Machine3 m3 = new Machine3();
+
+        m1.Start();
+        m1.Stop();
+
+        m2.Start();
+        m2.Stop();
+
+        m3.Start();
+        m3.Stop();
 
     }
 }
+
+
+public class Machine1:Icontrols
+{
+    public void Start()
+    {
+
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Machine2:Icontrols
+{
+    public void On()
+    {
+
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Machine3:Icontrols
+{
+    public void StartUp()
+    {
+
+    }
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+interface Icontrols
+{
+    void Start();
+    void Stop();
+}
+
+
